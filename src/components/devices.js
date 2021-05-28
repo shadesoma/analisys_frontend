dbdf001c56c6698259a985aee6951d778ec9738d
somakun <somabaka@gmail.com>
2021-05-28 20:05:15 +0300

    added device page

--- src/components/devices.js
+++ src/components/devices.js
@@ -1,11 +1,47 @@
 import React from 'react';
+import {
+  Tab,
+  Tabs,
+  Typography,
+} from '@material-ui/core';
+import CardsContainer from './cardsContainer';
+import SensorData from './sensorData';
+import Analytics from './analytics';
 
 const Devices = () => {
+  const [value, setValue] = React.useState(0);
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
   return (
-    <div>
-      Devices
+    <div className="devices" style={{ marginLeft: '10px' }}>
+      <Typography variant="h6"
+                  style={{ marginTop: '15px', marginBottom: '15px' }}>
+        Active Programs
+      </Typography>
+
+      <CardsContainer/>
+
+      <Tabs
+        value={value}
+        indicatorColor="primary"
+        textColor="primary"
+        onChange={handleChange}
+        aria-label="disabled tabs example"
+      >
+        <Tab label="Sensors Data"/>
+        <Tab label="Analytics"/>
+      </Tabs>
+
+      {value === 0 ? (
+        <SensorData/>
+      ) : (
+        <Analytics/>
+      )}
     </div>
   );
-};
+}
+;
 
 export default Devices;
