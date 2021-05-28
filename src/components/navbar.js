c5dafb014ff5c102d02a5be068fb6b2da43dee68
somakun <somabaka@gmail.com>
2021-05-28 17:56:17 +0300

    added navbar

---
+++ src/components/navbar.js
@@ -0,0 +1,113 @@
+import React from 'react';
+import {
+  AppBar, Avatar, Badge, IconButton, InputBase,
+  Toolbar, Typography,
+} from '@material-ui/core';
+import { makeStyles, fade } from '@material-ui/core/styles';
+import { Mail, Notifications, Search } from '@material-ui/icons';
+
+const useStyles = makeStyles((theme) => ({
+  grow: {
+    flexGrow: 1,
+  },
+  sectionDesktop: {
+    display: 'none',
+    [theme.breakpoints.up('md')]: {
+      display: 'flex',
+    },
+  },
+  search: {
+    position: 'relative',
+    borderRadius: theme.shape.borderRadius,
+    backgroundColor: fade(theme.palette.common.white, 0.15),
+    '&:hover': {
+      backgroundColor: fade(theme.palette.common.white, 0.25),
+    },
+    marginRight: theme.spacing(2),
+    marginLeft: 0,
+    width: '100%',
+    [theme.breakpoints.up('sm')]: {
+      marginLeft: theme.spacing(3),
+      width: 'auto',
+    },
+  },
+  searchIcon: {
+    padding: theme.spacing(0, 2),
+    height: '100%',
+    position: 'absolute',
+    pointerEvents: 'none',
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  inputRoot: {
+    color: 'inherit',
+  },
+  inputInput: {
+    padding: theme.spacing(1, 1, 1, 0),
+    // vertical padding + font size from searchIcon
+    paddingLeft: `calc(1em + ${theme.spacing(4)}px)`,
+    transition: theme.transitions.create('width'),
+    width: '100%',
+    [theme.breakpoints.up('md')]: {
+      width: '20ch',
+    },
+  },
+  name: {
+    color: 'white',
+    fontWeight: 500,
+    fontSize: 18,
+    marginLeft: 60,
+    marginTop: 15
+  }
+}));
+
+const Navbar = () => {
+  const classes = useStyles();
+  const menuId = 'primary-search-account-menu';
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <div className={classes.search}>
+          <div className={classes.searchIcon}>
+            <Search />
+          </div>
+          <InputBase
+            placeholder="Search…"
+            classes={{
+              root: classes.inputRoot,
+              input: classes.inputInput,
+            }}
+            inputProps={{ 'aria-label': 'search' }}
+          />
+        </div>
+
+        <div className={classes.grow} />
+        <div className={classes.sectionDesktop}>
+          <IconButton aria-label="show 4 new mails" color="inherit">
+            <Badge badgeContent={4} color="secondary">
+              <Mail />
+            </Badge>
+          </IconButton>
+          <IconButton aria-label="show 17 new notifications" color="inherit">
+            <Badge badgeContent={17} color="secondary">
+              <Notifications />
+            </Badge>
+          </IconButton>
+          <Typography className={classes.name}>Ivan Petrogradsky</Typography>
+          <IconButton
+            edge="end"
+            aria-label="account of current user"
+            aria-controls={menuId}
+            aria-haspopup="true"
+            color="inherit"
+          >
+            <Avatar alt="Ivan Petrogradsky" src="/icon/Avatar.png" />
+          </IconButton>
+        </div>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
--- src/components/root.js
+++ src/components/root.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Sidebar from './sidebar';
+import Navbar from './navbar';
 
 const Root = ({children = React.Children}) => {
   return (
     <section className="root" style={{display: 'flex', flexDirection: 'row'}}>
       <Sidebar />
-      <main>
+      <main style={{width: '100vw'}}>
+        <Navbar />
         {children}
       </main>
     </section>
